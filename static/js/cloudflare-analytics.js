fb23319800e7276f3a2fe48045ea1b7e1d0c256e
Miguel Branco <miguel@raw-labs.com>
2025-07-08 09:12:03 +0200

    Add Cloudflare Analytics via extend_head

--- static/js/cloudflare-analytics.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var s = document.createElement("script");
-  s.src = "https://static.cloudflareinsights.com/beacon.min.js";
-  s.defer = true;
-  s.setAttribute("data-cf-beacon", '{"token": "ba0363cc644f41eda2ace0d78e742f89"}');
-  document.head.appendChild(s);
-})();
